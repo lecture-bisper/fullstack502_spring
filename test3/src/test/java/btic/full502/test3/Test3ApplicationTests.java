26e7c13335b1474e9e58e3b14dc22d1d468d0bc2
lecture-bisper <bisper@outlook.kr>
2025-06-12 17:15:59 +0900

    Spring boot - start.spring.io 사이트를 사용한 스프링 MVC 방식 프로젝트 생성.

---
+++ test3/src/test/java/btic/full502/test3/Test3ApplicationTests.java
@@ -0,0 +1,13 @@
+package btic.full502.test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
