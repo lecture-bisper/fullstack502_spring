85bee4d3a5ae511f11a5fa35800f3b15f8e49379
lecture-bisper <bisper@outlook.kr>
2025-06-27 17:02:01 +0900

    Spring boot - xmljsonparser - open api 사용하기 (JAXB 라이브러리 추가)

---
+++ xmljsonparser/src/main/java/bitc/full502/xmljsonparser/XmljsonparserApplication.java
@@ -0,0 +1,13 @@
+package bitc.full502.xmljsonparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmljsonparserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XmljsonparserApplication.class, args);
+  }
+
+}
---
+++ xmljsonparser/src/test/java/bitc/full502/xmljsonparser/XmljsonparserApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.full502.xmljsonparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmljsonparserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
